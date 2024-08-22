e82bd75c0508baea7278c988e2e063a7ff36a3cd
Rafael Felipe <rafaelfelipe@Rafaels-MacBook-Air.local>
2024-08-22 16:50:21 -0300

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Header } from "@/sections/header";
-import { Hero } from "@/sections/hero";
-import { LogoTicker } from "@/sections/logoTicker";
-import { ProductShowcase } from "@/sections/productShowcase";
-import { Pricing } from "@/sections/pricing";
-import { Testimonials } from "@/sections/testimonials";
-import { CallToAction } from "@/sections/callToAction";
-import { Footer } from "@/sections/footer";
+import { Header } from "@/sections/Header";
+import { Hero } from "@/sections/Hero";
+import { LogoTicker } from "@/sections/LogoTicker";
+import { ProductShowcase } from "@/sections/ProductShowcase";
+import { Pricing } from "@/sections/Pricing";
+import { Testimonials } from "@/sections/Testimonials";
+import { CallToAction } from "@/sections/CallToAction";
+import { Footer } from "@/sections/Footer";
 
 export default function Home() {
   return (

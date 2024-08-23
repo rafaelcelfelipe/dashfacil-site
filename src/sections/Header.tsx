09a5a8d06eec437f36f6e5c479dfe851c3567e35
Rafael Felipe <rafaelfelipe@Rafaels-MacBook-Air.local>
2024-08-23 07:54:29 -0300

    correcao imagens

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -18,7 +18,7 @@ export const Header = () => {
       <div className="py-5">
         <div className="container">
           <div className="flex items-center justify-between">
-            <Image src={Logo.src} alt="Saas Logo" height={40} width={40} />
+            <Image src={'src/assets/logosaas.png'} alt="Saas Logo" height={40} width={40} />
             <MenuIcon className="h-5 w-5 md:hidden" />
             <nav className="hidden md:flex gap-6 text-black/60 items-center">
               <a href="#">About</a>

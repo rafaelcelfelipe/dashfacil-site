80f09ca1bb5c1c7578e68a083753b593c4cdc10b
Rafael Felipe <rafaelfelipe@Rafaels-MacBook-Air.local>
2024-11-27 14:25:44 -0300

    correcao politica de privacidade

--- src/app/politicas-de-privacidade/page.tsx
+++ src/app/politicas-de-privacidade/page.tsx
@@ -1,41 +1,64 @@
 export default function Politicas() {
     return (
         <div className="bg-white py-5 px-5">
-            <h2 className="text-lg font-bold mb-5 mt-5"><span>Política Privacidade</span></h2>
-            <p className="mb-5"><span>A sua privacidade é importante para nós. É política do Dash Fácil respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site <a href="https://dashfacil.com">Dash Fácil</a>, e outros sites que possuímos e operamos.</span></p>
-            <p className="mb-5"><span>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</span></p>
-            <p className="mb-5"><span>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.</span></p>
-            <p className="mb-5"><span>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</span></p>
-            <p className="mb-5"><span>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.</span></p>
-            <p className="mb-5"><span>Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.</span></p>
-            <p className="mb-5"><span>O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.</span></p>
-            <h3><span>Uso das APIs do Google Workspace</span></h3>
+            <h2 className="text-lg font-bold mb-5 mt-5"><span>Política de Privacidade</span></h2>
+            <p className="mb-5"><span>A sua privacidade é importante para nós. É política do Dash Fácil respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site <a href="https://dashfacil.com">Dash Fácil</a> e outros sites que possuímos e operamos.</span></p>
+
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Informações Pessoais Coletadas</span></h3>
+            <p className="mb-5"><span>Nosso aplicativo coleta e utiliza as seguintes informações pessoais dos usuários:</span></p>
+            <ul className="list-disc ml-5 mb-5">
+                <li><span><strong>Contas de anúncios disponíveis no Google Ads:</strong> Inclui informações sobre as contas de anúncios associadas ao usuário, como IDs de conta e nomes.</span></li>
+                <li><span><strong>Dados de campanhas e anúncios do Google Ads:</strong> Coletamos dados relacionados às campanhas publicitárias e anúncios, incluindo métricas de desempenho, configurações de campanha e outros detalhes relevantes.</span></li>
+                <li><span><strong>Formulários disponíveis no Google Drive:</strong> Acessamos a lista de formulários que o usuário possui ou tem acesso no Google Drive.</span></li>
+                <li><span><strong>Dados de formulários, incluindo perguntas e respostas:</strong> Coletamos os dados dos formulários selecionados pelo usuário, o que pode incluir perguntas formuladas e respostas fornecidas pelos respondentes.</span></li>
+            </ul>
+
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Uso das Informações Coletadas</span></h3>
+            <p className="mb-5"><span>Utilizamos as informações coletadas para:</span></p>
+            <ul className="list-disc ml-5 mb-5">
+                <li><span>Fornecer os serviços solicitados pelo usuário, como análise de dados de campanhas publicitárias e gerenciamento de formulários.</span></li>
+                <li><span>Melhorar e personalizar a experiência do usuário em nosso aplicativo.</span></li>
+            </ul>
+
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Armazenamento e Proteção dos Dados</span></h3>
+            <p className="mb-5"><span>Os dados coletados são armazenados de forma segura, com medidas técnicas e organizacionais apropriadas para proteger contra acesso não autorizado, alteração, divulgação ou destruição.</span></p>
+            <p className="mb-5"><span>Retemos os dados apenas pelo tempo necessário para cumprir as finalidades para as quais foram coletados, a menos que um período de retenção mais longo seja exigido ou permitido por lei.</span></p>
+
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Compartilhamento de Dados</span></h3>
+            <p className="mb-5"><span>Não compartilhamos informações pessoais dos usuários com terceiros, exceto conforme exigido por lei ou com o consentimento explícito do usuário.</span></p>
+            <p className="mb-5"><span>Podemos compartilhar dados agregados e anonimizados que não identificam individualmente os usuários para fins de análise e melhoria de nossos serviços.</span></p>
+
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Consentimento e Controle do Usuário</span></h3>
+            <p className="mb-5"><span>Antes de acessar e coletar dados do usuário, solicitamos autorização explícita por meio dos processos de consentimento do Google OAuth.</span></p>
+            <p className="mb-5"><span>O usuário pode revogar a permissão a qualquer momento através das configurações de segurança de sua conta Google.</span></p>
+
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Uso das APIs do Google Workspace</span></h3>
             <p className="mb-5"><span>Confirmamos que não utilizamos as APIs do Google Workspace para desenvolver, melhorar ou treinar modelos generalizados de IA e/ou ML.</span></p>
-            <h3><span>Dados do Usuário do Google</span></h3>
-            <p className="mb-5"><span>Nosso aplicativo acessa dados específicos do Google para fornecer a funcionalidade prometida. Em particular, acessamos:</span></p>
-            <ul>
-                <li><span>Contas de anúncios disponíveis no Google Ads;</span></li>
-                <li><span>Dados de campanhas e anúncios do Google Ads;</span></li>
-                <li><span>Listas de formulários armazenadas no Google Drive;</span></li>
-                <li><span>Dados de formulários, incluindo perguntas e respostas.</span></li>
-            </ul>
-            <p className="mb-5"><span>Utilizamos esses dados exclusivamente para fornecer os serviços descritos e conforme autorizado pelo usuário. Não utilizamos os dados para quaisquer outros fins, incluindo desenvolvimento, melhoria ou treinamento de modelos de IA ou ML generalizados.</span></p>
-            <h3><span>Uso, Armazenamento e Compartilhamento de Dados do Usuário</span></h3>
-            <p className="mb-5"><span>Os dados do usuário do Google que acessamos são usados exclusivamente para fornecer e melhorar os serviços descritos, de acordo com a autorização do usuário.</span></p>
-            <p className="mb-5"><span>Esses dados são armazenados de forma segura e com proteções adequadas, enquanto forem necessários para os propósitos descritos. Não compartilhamos esses dados com terceiros, exceto conforme exigido por lei ou para fornecer serviços com o consentimento explícito do usuário.</span></p>
-            <h3><span>Compromisso do Usuário</span></h3>
+
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Conformidade com as Políticas do Google</span></h3>
+            <p className="mb-5"><span>Nosso aplicativo está em conformidade com as políticas de dados do usuário dos Serviços de API do Google e não utiliza os dados para nenhum outro propósito além dos descritos nesta política.</span></p>
+
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Direitos do Usuário</span></h3>
+            <p className="mb-5"><span>O usuário tem o direito de acessar, corrigir, atualizar ou solicitar a exclusão de suas informações pessoais a qualquer momento.</span></p>
+            <p className="mb-5"><span>Para exercer esses direitos, o usuário pode entrar em contato conosco através dos canais fornecidos nesta política.</span></p>
+
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Compromisso do Usuário</span></h3>
             <p className="mb-5"><span>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Dash Fácil oferece no site e com caráter enunciativo, mas não limitativo:</span></p>
-            <ul>
-                <li><span>A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;</span></li>
-                <li><span>B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;</span></li>
-                <li><span>C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do Dash Fácil, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.</span></li>
+            <ul className="list-disc ml-5 mb-5">
+                <li><span>A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé e à ordem pública;</span></li>
+                <li><span>B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, qualquer tipo de pornografia ilegal, apologia ao terrorismo ou contra os direitos humanos;</span></li>
+                <li><span>C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do Dash Fácil, de seus fornecedores ou terceiros, nem introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.</span></li>
             </ul>
-            <h3><span>Alterações na Política de Privacidade</span></h3>
+
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Alterações na Política de Privacidade</span></h3>
             <p className="mb-5"><span>Reservamo-nos o direito de atualizar esta política de privacidade periodicamente. Quando realizarmos mudanças, notificaremos os usuários através de email. Recomendamos revisar esta política periodicamente para se manter informado sobre nossas práticas.</span></p>
 
-            <h3><span>Mais informações</span></h3>
-            <p className="mb-5"><span>Esperamos que esta política esteja clara e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos que você usa em nosso site.</span></p>
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Mais Informações</span></h3>
+            <p className="mb-5"><span>Esperamos que esta política esteja clara e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados caso interaja com um dos recursos que você usa em nosso site.</span></p>
             <p className="mb-5"><span>Esta política é efetiva a partir de 27 de agosto de 2024, 11:33.</span></p>
+
+            <h3 className="text-md font-bold mb-3 mt-5"><span>Contato</span></h3>
+            <p className="mb-5"><span>Se você tiver alguma dúvida ou preocupação sobre nossa política de privacidade ou sobre o tratamento de seus dados pessoais, entre em contato conosco pelo email: contato@dashfacil.com.</span></p>
         </div>
     )
 }

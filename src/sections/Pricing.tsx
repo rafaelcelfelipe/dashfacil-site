36d4889b67a0e434e25ca5e2c2fbdc826743da01
Rafael Felipe <rafaelfelipe@Rafaels-MacBook-Air.local>
2024-12-04 15:54:41 -0300

    novos links de checkout

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -19,7 +19,8 @@ const pricingTiers = [
       "Criação de dashboards",
       "Atualização automática",
     ],
-    checkout: "https://buy.stripe.com/test_14k4kg8bqejz4ak5kk",
+    checkoutMonthly:
+      "https://payment-link-sdx.pagar.me/pl_bqaQnzLvw09ZAanIMi9DZJrj1Nm543E7",
   },
   {
     title: "Avançado",
@@ -35,7 +36,8 @@ const pricingTiers = [
       "Ferramentas com IA",
       "Criador de debriefing",
     ],
-    checkout: "https://buy.stripe.com/test_aEU5okdvK8ZfgX67st",
+    checkoutMonthly:
+      "https://payment-link-sdx.pagar.me/pl_48rxRdkLXY3g1kJfysnwEDBMePyZWalp",
   },
   {
     title: "Empresas",
@@ -51,20 +53,21 @@ const pricingTiers = [
       "Ferramentas com IA",
       "Criador de debriefing",
     ],
-    checkout: "https://buy.stripe.com/test_fZe4kgajyfnD9uEaEG",
+    checkoutMonthly:
+      "https://payment-link-sdx.pagar.me/pl_AjZG8D457Lb0XV5mFVTnqrep1kaW6PxO",
   },
 ];
 
 export const Pricing = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [email, setEmail] = useState("");
-  const [selectedCheckoutUrl, setSelectedCheckoutUrl] = useState("");
+  const [selectedUrl, setSelectedUrl] = useState("");
   const [emailError, setEmailError] = useState("");
   const router = useRouter();
   const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-  const handleOpenModal = (checkoutUrl: string) => {
-    setSelectedCheckoutUrl(checkoutUrl);
+  const handleOpenModal = (checkoutMonthly:string) => {
+    setSelectedUrl(checkoutMonthly);
     setIsModalOpen(true);
     setEmail("");
     setEmailError("");
@@ -77,9 +80,10 @@ export const Pricing = () => {
     }
 
     try {
-      const url = new URL('/api/check-email', backendUrl);
+      // Verifica se o email já existe
+      const checkEmailUrl = new URL('/api/check-email', backendUrl);
 
-      const response = await fetch(url.toString(), {
+      const checkEmailResponse = await fetch(checkEmailUrl.toString(), {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -87,51 +91,37 @@ export const Pricing = () => {
         body: JSON.stringify({ email }),
       });
 
-      const data = await response.json();
+      const checkEmailData = await checkEmailResponse.json();
 
-      if (data.exists) {
+      if (checkEmailData.exists) {
         setEmailError(
           "Este email já está em uso. Se você é o dono da conta e quer alterá-la, acesse app.dashfacil.com/minha-conta. Caso contrário, use outro email."
         );
       } else {
-        const url = new URL(selectedCheckoutUrl);
-        url.searchParams.append("prefilled_email", email);
-
+        // Redireciona para o link de checkout
         setIsModalOpen(false);
-        router.push(url.toString());
+        router.push(selectedUrl);
       }
     } catch (error) {
-      console.error("Erro ao verificar o email:", error);
-      alert("Ocorreu um erro ao verificar o email. Por favor, tente novamente.");
+      console.error("Erro ao processar a solicitação:", error);
+      alert("Ocorreu um erro. Por favor, tente novamente.");
       setIsModalOpen(false);
     }
   };
 
   return (
-    <section className="py-24 bg-white">
-      <div className="container">
-        <div className="section-heading">
-          <h2 className="section-title">Planos</h2>
-          <p className="section-description mt-5">
-            Escolha o que mais se encaixa com você
-          </p>
-        </div>
-        <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
-          {pricingTiers.map(
-            (
-              {
-                title,
-                monthlyPrice,
-                buttonText,
-                popular,
-                inverse,
-                features,
-                checkout,
-              },
-              index
-            ) => (
+    <>
+      <section className="py-24 bg-white">
+        <div className="container">
+          <div className="section-heading">
+            <h2 className="section-title">Planos</h2>
+            <p className="section-description mt-5">
+              Escolha o que mais se encaixa com você
+            </p>
+          </div>
+          <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
+            {pricingTiers.map(({checkoutMonthly, inverse, title, popular, monthlyPrice, buttonText, features}) => (
               <div
-                key={index}
                 className={twMerge(
                   "card",
                   inverse === true && "border-black bg-black text-white"
@@ -158,7 +148,7 @@ export const Pricing = () => {
                           ease: "linear",
                           repeatType: "loop",
                         }}
-                        className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] [background-size:200%] text-transparent bg-clip-text font-medium"
+                        className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] [background-size:200%] text-transparent bg-clip-text font-medium"
                       >
                         Popular
                       </motion.span>
@@ -179,7 +169,7 @@ export const Pricing = () => {
                   </span>
                 </div>
                 <button
-                  onClick={() => handleOpenModal(checkout)}
+                  onClick={() => handleOpenModal(checkoutMonthly)}
                   className={twMerge(
                     "btn btn-primary w-full mt-[30px] cursor-pointer",
                     inverse === true && "bg-white text-black"
@@ -188,49 +178,48 @@ export const Pricing = () => {
                   {buttonText}
                 </button>
                 <ul className="flex flex-col gap-5 mt-8">
-                  {features.map((feature, index) => (
-                    <li key={index} className="text-sm flex items-center gap-4">
+                  {features.map((feature, idx) => (
+                    <li key={idx} className="text-sm flex items-center gap-4">
                       <CheckIcon className="h-6 w-6" />
                       <span>{feature}</span>
                     </li>
                   ))}
                 </ul>
               </div>
-            )
-          )}
-        </div>
-      </div>
-
-      {/* Modal para coletar o email */}
-      {isModalOpen && (
-        <Modal onClose={() => setIsModalOpen(false)}>
-          <div className="p-6">
-            <h2 className="text-xl font-bold mb-4">Insira seu email</h2>
-            <input
-              type="email"
-              className={twMerge(
-                "w-full mb-2 border px-2 py-1 rounded-lg",
-                emailError && "border-red-500"
-              )}
-              placeholder="seuemail@exemplo.com"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-                if (emailError) setEmailError("");
-              }}
-            />
-            {emailError && (
-              <p className="text-red-500 text-sm mb-4">{emailError}</p>
-            )}
-            <button
-              className="btn btn-primary w-full"
-              onClick={handleEmailSubmit}
-            >
-              Prosseguir
-            </button>
+            ))}
           </div>
-        </Modal>
-      )}
-    </section>
+        </div>
+
+        {/* Modal para coletar o email */}
+        {isModalOpen && (
+          <Modal onClose={() => setIsModalOpen(false)}>
+            <div className="p-6">
+              <input
+                type="email"
+                className={twMerge(
+                  "w-full mb-2 border px-2 py-1 rounded-lg",
+                  emailError && "border-red-500"
+                )}
+                placeholder="seuemail@exemplo.com"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  if (emailError) setEmailError("");
+                }}
+              />
+              {emailError && (
+                <p className="text-red-500 text-sm mb-4">{emailError}</p>
+              )}
+              <button
+                className="btn btn-primary w-full"
+                onClick={handleEmailSubmit}
+              >
+                Prosseguir
+              </button>
+            </div>
+          </Modal>
+        )}
+      </section>
+    </>
   );
 };
